89012160717f5cb147d31fd66f635e04d1104f25
Casper <casper-s@hotmail.dk>
2020-10-23 13:04:17 +0200

    Markers and polylines

--- RunTogether/JsLib/src/index.js
+++ RunTogether/JsLib/src/index.js
@@ -8,12 +8,21 @@ let mymap;
 
 export function leaflet_start() {
 
-    mymap = L.map('mapid').setView([51.505, -0.09], 13);
+    // Start view for map (zoomlevel and viewpoint)
+    mymap = L.map('mapid').setView([57.0117789, 9.9907118], 6);
 
-    var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    var Stamen_Watercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        /*subdomains: 'abcd',*/
+        minZoom: 6,
+        maxZoom: 18,
+        ext: 'jpg'
     }).addTo(mymap);
+
+    //var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    //    maxZoom: 19,
+    //    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    //}).addTo(mymap);
 
     /*    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -32,7 +41,16 @@ export function leaflet_start() {
 
 export function addMarker() {
 
-    L.marker([51.5, -0.09]).addTo(mymap);
+    L.marker([57.0117789, 9.9907118]).addTo(mymap);
+    L.marker([57.0123239, 9.9939051]).addTo(mymap);
+
+    var latlngs = [
+        [57.0117789, 9.9907118],
+        [57.0123239, 9.9939051]
+    ];
+
+    var polyline = L.polyline(latlngs, { color: 'red' }).addTo(mymap);
+
 
     console.log("EN marker? tak");
 }
