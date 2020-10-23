c16c45383327a19a7e48e9a841de84f6514b3e74
frederikhm <1637511+frederikhm@users.noreply.github.com>
2020-10-23 15:13:22 +0200

    added Radzen and a table to home page

---
+++ RunTogether/Run.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RunTogether
+{
+
+    public class Run
+    {
+        public int distance { get; set; }
+
+        public string name { get; set; }
+
+        public string city { get; set; }
+    }
+}
