d9d882f4cd4fbb79098ef672ce2c2dc90787e388
Oliver Harold <oliverhsj@hotmail.dk>
2020-10-26 13:47:49 +0100

    Added Account Identity

--- RunTogether/Startup.cs
+++ RunTogether/Startup.cs
@@ -36,6 +36,7 @@ namespace RunTogether
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
             services.AddServerSideBlazor();
