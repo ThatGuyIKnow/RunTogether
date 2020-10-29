a04e82625be108836268c456b664d615faa7538d
Magnus Mathiesen <magnus.mathiesen@yahoo.com>
2020-10-29 13:39:04 +0100

    All sponsor images are now of same height.
    This does not stretch or clip the images.

--- RunTogether/JsLib/src/SidebarHide.js
+++ RunTogether/JsLib/src/SidebarHide.js
@@ -3,7 +3,7 @@
     /* Open or close the sidebar */
     sidebarToggle(style) {
 
-        /* Gets object like so: {id: myElement, attribute: width, value: 200px} */
+        /* Gets object like so: { id: "myElement", attribute: "width", value: "200px" } */
         document.getElementById(style.id).style[style.attribute] = style.value;
     }
 }
