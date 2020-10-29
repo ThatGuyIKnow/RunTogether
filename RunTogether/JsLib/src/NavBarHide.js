324dbc47343f976743eaffe6f3705f9b0eb3759b
Magnus Mathiesen <magnus.mathiesen@yahoo.com>
2020-10-29 12:57:52 +0100

    Sponsor logos are now pulled form a list, no longer hardcoded

--- RunTogether/JsLib/src/NavBarHide.js
+++ RunTogether/JsLib/src/NavBarHide.js
@@ -2,11 +2,13 @@
 {
     /* Close the sidebar */
     navbarToggleOpen(style) {
+
         document.getElementById(style.id).style[style.attribute] = style.value;
     }
 
     /* Open the sidebar */
     navbarToggleClose(style) {
+
         document.getElementById(style.id).style[style.attribute] = style.value;
     }
 }
