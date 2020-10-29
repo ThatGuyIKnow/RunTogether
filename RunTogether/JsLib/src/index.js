4d51b9715c11e88e10fe8c62e08024f76f7c8640
Casper <casper-s@hotmail.dk>
2020-10-29 08:00:28 +0100

    Create Function creating markers

--- RunTogether/JsLib/src/index.js
+++ RunTogether/JsLib/src/index.js
@@ -17,33 +17,54 @@ let mymap
 //Coordinates for the  polyline
 let latlngs = [
     [57.0117789, 9.9907118],
-    [57.00967, 10.00404],
-    [58.0123239, 9.9940051]
+    [56.7499, 9.9921],
+    [56.467, 9.2708], 
+    [56.0221, 9.2288], 
+    [55.6123, 9.1428]   
 ];
 
 export function leaflet_start() {
 
+    let startZoomPoint = 6,
+        minZoomL = 6,
+        maxZoomL = 13; 
+
     // Start view for map (zoomlevel and viewpoint)
-    mymap = L.map('mapid').setView([57.0117789, 9.9907118], 6);
+    mymap = L.map('mapid').setView([57.0117789, 9.9907118], startZoomPoint);
 
     L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
         attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        /*subdomains: 'abcd',*/
-        minZoom: 6,
-        maxZoom: 16,
+        subdomains: 'abcd',
+        minZoom: minZoomL, 
+        maxZoom: maxZoomL,
         ext: 'jpg'
     }).addTo(mymap);
 
-    addMarkers(); 
+    addMarkers(latlngs); 
     addPolyline(latlngs); 
 
     console.log("leaflet_start() function is done ");
+
 }
 
-function addMarkers() {
-    L.marker([57.0117789, 9.9907118]).bindPopup('Start for segment 1<br/>Dette segment er sponseret af State.</p><br/><img src="/logos/State_Logo_v1.jpg" asp-append-version="true" width="300px" />').openPopup().addTo(mymap);
+function addMarkers(latlngs) {
+    let i = 0; 
+    let segNum = 0; 
+
+    for (i = 0; i < latlngs.length; i++) {
+        segNum = i + 1;
+        let marker =L.marker(latlngs[i]).bindPopup('Start for segment ' + segNum +
+                                        '<br />Dette segment er sponseret af [SPONSOR].</p>').openPopup().addTo(mymap); 
+       /* mymap.addLayer(marker);*/
+    }
+    /*mymap.removeLayer(marker);*/ 
+
+/*
+    let marker1 = L.marker([57.0117789, 9.9907118]).bindPopup('Start for segment 1<br/>Dette segment er sponseret af State.</p><br/><img src="/logos/State_Logo_v1.jpg" asp-append-version="true" width="300px" />').openPopup().addTo(mymap);
+    mymap.addLayer(marker1);
+    mymap.removeLayer(marker1); 
     L.marker([57.00967, 10.00404]).bindPopup('Start for segment 2<br />Dette segment er sponseret af FrugtKurven.</p><br/><img src="/logos/Frugtkurven_Logo.png" asp-append-version="true" width="300px" />').openPopup().addTo(mymap);
-    L.marker([58.0123239, 9.9940051]).bindPopup('Start for segment 3<br />Dette segment er sponseret af FrugtKurven.</p><br/><img src="/logos/Frugtkurven_Logo.png" asp-append-version="true" width="300px" />').openPopup().addTo(mymap);
+    L.marker([58.0123239, 9.9940051]).bindPopup('Start for segment 3<br />Dette segment er sponseret af FrugtKurven.</p><br/><img src="/logos/Frugtkurven_Logo.png" asp-append-version="true" width="300px" />').openPopup().addTo(mymap);*/
 }
 
 function addPolyline(latlngs) {
