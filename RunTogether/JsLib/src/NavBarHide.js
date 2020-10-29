149e3304ff14475ab78c06a270a20110eac73b65
Magnus Mathiesen <magnus.mathiesen@yahoo.com>
2020-10-29 01:01:07 +0100

    Got JSInterop working, added navToggle functions

--- RunTogether/JsLib/src/NavBarHide.js
+++ RunTogether/JsLib/src/NavBarHide.js
@@ -1,1 +1,9 @@
-﻿
+﻿/* Close the sidebar */
+function navbarToggleOpen() {
+    document.getElementById("sidebarId").style.width = 200;
+}
+
+/* Open the sidebar */
+function navbarToggleClose() {
+    document.getElementById("sidebarId").style.width = 0;
+}
