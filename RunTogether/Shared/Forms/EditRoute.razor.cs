824a9a46d4c5fa1b117657cb6ca3b1c4caf6143d
Karin <karin_wallsten@hotmail.com>
2020-10-30 14:38:35 +0100

    Added partial class editroute

---
+++ RunTogether/Shared/Forms/EditRoute.razor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RunTogether.Shared.Forms
+{
+    public partial class EditRoute
+    {
+        IEnumerable<Stage> stages;
+
+
+        //protected override void OnInitialized()
+        //{
+        //    //customers = dbContext.Customers.ToList(); <- exempel fra https://blazor.radzen.com/dropdown
+
+        //    stages = applicationDbContext.Stages.ToList();  //måske?
+        //}
+
+        string value;
+
+        Dictionary<DateTime, string> events = new Dictionary<DateTime, string>();
+
+        void Change(string value, string name)
+        {
+            events.Add(DateTime.Now, $"{name} value changed to {value}");
+            StateHasChanged();
+        }
+
+        void ChangeBound(object value, string name)
+        {
+            events.Add(DateTime.Now, $"{name} value changed to {value}");
+            StateHasChanged();
+        }
+    }
+}
