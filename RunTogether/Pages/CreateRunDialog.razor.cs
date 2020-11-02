54b4878f052868f065fc26dcca976b326673defd
Magnus Mathiesen <magnus.mathiesen@yahoo.com>
2020-11-02 09:21:38 +0100

    Added partial classes, deleted deprecated files

---
+++ RunTogether/Pages/CreateRunDialog.razor.cs
@@ -0,0 +1,21 @@
+﻿using Radzen;
+using System;
+
+namespace RunTogether.Pages
+{
+    public partial class CreateRunDialog
+    {
+        Run run = new Run();
+
+        void OnSubmit(Run run)
+        {
+            Console.WriteLine("YEY!");
+            this.dialogService.Close(true);
+        }
+
+        void OnInvalidSubmit(FormInvalidSubmitEventArgs args)
+        {
+            Console.WriteLine("AW");
+        }
+    }
+}
