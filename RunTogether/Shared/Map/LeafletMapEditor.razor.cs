cebe03b9b6b3765db42bd22c2afacccfe8b7a322
Casper <casper-s@hotmail.dk>
2020-11-05 09:47:29 +0100

    Components + change CSS for Map

---
+++ RunTogether/Shared/Map/LeafletMapEditor.razor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+using RunTogether.Data;
+
+
+
+namespace RunTogether.Shared.Map
+{
+    public partial class LeafletMapEditor
+    {
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+
+
+            //await JsRunTime.InvokeVoidAsync("Main.tester", json);
+
+            if (firstRender)
+            {
+                await JsRunTime.InvokeVoidAsync("Main.Map.initializeMap");
+                StateHasChanged();
+            }
+        }
+
+
+        private async void CreateMarker()
+        {
+            await JsRunTime.InvokeVoidAsync("Main.Map.onMapClick");
+        }
+
+
+    }
+
+}
