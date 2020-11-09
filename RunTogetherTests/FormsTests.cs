e6f01816bfd5d8b8993c8f718244ef3cf6323e51
Magnus Mathiesen <magnus.mathiesen@yahoo.com>
2020-11-09 10:17:13 +0100

    Added draft for FormTests

--- RunTogetherTests/FormsTests.cs
+++ RunTogetherTests/FormsTests.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using Xunit;
 using Bunit;
+using RunTogether.Shared.Forms;
 
 namespace RunTogetherTests
 {
     public class FormsTests : TestContext
     {
+        //// Test the change function
+        //[Fact]
+        //public void Change_Does_Change()
+        //{
+        //    // Arrange
+        //    CreateNewRunner change = new CreateNewRunner();
+
+        //    // Act
+        //    string result = change.Change();
+
+        //    // Assert
+
+        }
     }
 }
