369e0aafeae69227fdde08b5f745caae21956e26
Oliver Harold <oliverhsj@hotmail.dk>
2020-11-10 12:42:27 +0100

    EventHandlerHelper, untested

---
+++ RunTogether/Shared/Etc/Helpers/EventHandlerHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.JSInterop;
+
+namespace RunTogether.Shared.Etc.Helpers
+{
+    public class EventHandlerHelper : IDisposable
+    {
+        public readonly DotNetObjectReference<EventHandlerHelper> ObjRef;
+
+        private readonly Dictionary<string, Action<object>> _eventHandlers = new Dictionary<string, Action<object>>();
+        
+        public EventHandlerHelper()
+        {
+            ObjRef = DotNetObjectReference.Create(this);
+        }
+
+        public void AddHandler(string eventTrigger, Action<object> handler)
+        {
+            string eventTriggerNorm = eventTrigger.ToUpper();
+            _eventHandlers.Add(eventTriggerNorm, handler);
+        }
+
+        public bool RemoveHandler(string eventTrigger)
+        {
+            string eventTriggerNorm = eventTrigger.ToUpper();
+            return _eventHandlers.Remove(eventTriggerNorm);
+        }
+
+        [JSInvokable]
+        public void Trigger(string eventTrigger, object data)
+        {
+            string eventTriggerNorm = eventTrigger.ToUpper();
+            try
+            {
+                _eventHandlers[eventTriggerNorm].Invoke(data);
+            }
+            catch (KeyNotFoundException e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public void Dispose()
+        {
+            ObjRef.Dispose();
+        }
+    }
+}
