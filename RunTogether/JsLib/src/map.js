5890115500041d18e1efe7ecc58d7b4e14a416aa
Casper <casper-s@hotmail.dk>
2020-11-12 10:05:57 +0100

    Added bounds again

--- RunTogether/JsLib/src/map.js
+++ RunTogether/JsLib/src/map.js
@@ -1,10 +1,10 @@
 ﻿
 /*Global variable for the map class*/
-let mymap, layerGroup, polyline;
-/*let maxBounds1 = [51.649, 0.49]; 
+let mymap, layerGroup;
+let maxBounds1 = [51.649, 0.49]; 
 let maxBounds2 = [59.799, 18.68];  
 let bounds = L.latLngBounds(maxBounds1, maxBounds2);
-*/
+
 /*Class for the map*/
 export class mapClass {
 
@@ -24,16 +24,16 @@ export class mapClass {
         /*Pointing mymap to leaflet map and setting the viewpoint and start zoom point*/
         mymap = L.map('mapid').setView([55.964, 9.992], 6.5);
 
-/*        mymap.setMaxBounds(bounds);
-*/
+        mymap.setMaxBounds(bounds);
+
         /* Appling tile layer to the map*/
         L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
             attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
             subdomains: 'abcd',
             minZoom: 6,
             maxZoom: 13,
-/*            maxBounds: bounds,
-            maxBoundsViscosity: 1, */
+            maxBounds: bounds,
+            maxBoundsViscosity: 1, 
             ext: 'jpg'
         }).addTo(mymap);
 
