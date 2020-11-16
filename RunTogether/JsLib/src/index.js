038fd254e63afc4c69aca845561118462a798a79
Oliver Harold <oliverhsj@hotmail.dk>
2020-11-16 12:24:24 +0100

    AbstractMarker

--- RunTogether/JsLib/src/index.js
+++ RunTogether/JsLib/src/index.js
@@ -11,19 +11,11 @@ export const Common = new CommonJS();
 export const QrScanner = new QrScannerClass();
 export const Sidebar = new SidebarCollapseFunctionality();
 
-//export async function tester(object) {
-//    console.log(object);
-    
-//    latlngs = await JSON.parse(object).Coordinates;
-
-//    console.log(latlngs);
-//}
-
 let Route = {
+	Name: "RunTogetherTest",
 	Stages: [
 		{
 			StartPoint: [57.0405, 9.9101],
-			/*			GuidePoint: [[57.0383, 9.9057], [57.0289, 9.9122]], */
 			Throughpoint: [[57.0342, 9.9089]],
 			EndPoint: [57.0257, 9.9062],
 			Status: "Active",
@@ -31,19 +23,11 @@ let Route = {
 				     { Status: "active", Name: "Puma", Order: 1 }],
 			Sponsor: {
 				Name: "Lejenregnskabschef.dk",
-				/*PictureURL: "https://www.runtogether.dk/wp-content/uploads/2020/03/jhuih.png",*/
+				PictureURL: "https://www.runtogether.dk/wp-content/uploads/2020/03/jhuih.png",
 				Message: "Vi er stolte sponsorer"
 			}
 		}
 	]
 }	
-/*let i, stagenumber = 0;
-for (i = 0; i < Route.Stages.length; i++) {
-	stagenumber++;
-	console.log("Stage: " + stagenumber +
-		", Runner: " + Route.Stages[i].Runner.Name +
-		", Sponsor: " + Route.Stages[i].Sponsor.Name +
-		", Sponsor besked: " + Route.Stages[i].Sponsor.Message); 
-}
-*/
+
 export const Map = new mapClass(Route);
--- RunTogether/JsLib/src/map.js
+++ RunTogether/JsLib/src/map.js
@@ -16,12 +16,11 @@ export class mapClass {
     constructor() {
         this.initializeMap = this.initializeMap.bind(this);
         this.addMarkersAndLines = this.addMarkersAndLines.bind(this);
-        //this.calulateControlPoints = this.calulateControlPoints.bind(this); 
         this.removeMarkersAndLines = this.removeMarkersAndLines.bind(this);
     }
 
     /* A Method that initializes the map */
-    initializeMap() {
+    initializeMap() {   
 
         /*Pointing mymap to leaflet map and setting the viewpoint and start zoom point*/
         mymap = L.map('mapid').setView([55.964, 9.992], 6.5);
@@ -47,199 +46,35 @@ export class mapClass {
     /* A Method to add markers and lines*/
     addMarkersAndLines(object) {
 
-        this.removeMarkersAndLines();
-
-        let obj = JSON.parse(object);
-
-        console.log(object);
-        console.log(obj);
-
-        let i = 0, j = 0, n = 0, curvedLine, lineArray = [], runnerName = [];
 
         /*Creating layer group and adding to map*/
         layerGroup = L.layerGroup().addTo(mymap);
 
-        /*Creates curvedlies from object with stages*/
-        /* for (i = 0; i < this.obj.Stages.length; i++) {
-
-            let startpoint = this.obj.Stages[i].StartPoint;
-            let endpoint = this.obj.Stages[i].EndPoint
-            let throughpoint, guidepoint;
-
-            lineArray.push('M', startpoint,);
-
-             for (j = 0; j < this.obj.Stages[i].Throughpoint.length; j++) {
-
-                if (j == this.obj.Stages[i].Throughpoint.length) {
-                    throughpoint = this.obj.Stages[i].Throughpoint[j];
-                    guidepoint = this.obj.Stages[i].GuidePoint[j];
-                    lineArray.push('Q', guidepoint, throughpoint,);
-                } else {
-                    guidepoint = this.obj.Stages[i].GuidePoint[j];
-                    lineArray.push('Q', guidepoint, endpoint);
-                }
-
-                console.log(lineArray);
-             }
-
-            for (n = 0; n > this.obj.Stages[i].GuidePoint[j]) {
-
-            }
-
-            L.marker(startpoint).addTo(mymap);
-            L.marker(endpoint).addTo(mymap);
-
-            curvedLine = L.curve(lineArray, { color: 'red' }).bindPopup('Dette er noget tekst').openPopup();
-
-            layerGroup.addLayer(curvedLine).addTo(mymap); 
-
-            mymap.fitBounds(curvedLine.getBounds());
-
-
-            calulateControlPoints(latlng1, latlng2); 
-
-        }
+/*      mymap.fitBounds(curvedLine.getBounds());
 */
-        //let latlng1 = [57.0405, 9.9101];
-        //let latlng2 = [57.0257, 9.9062];
-        //let latlng3 = [57.0039, 9.9027];
 
-        //let midtpunkt1 = this.calulateControlPoints(latlng1, latlng2, "right"); 
-        //let midtpunkt2 = this.calulateControlPoints(latlng3, latlng4, "left"); 
-
-        //let curvedPath = L.curve(
-        //    [
-        //        'M', latlng1,
-        //        'Q', midtpunkt1, latlng2,
-        //        'Q', midtpunkt2, latlng3
-        //    ], { color: 'red' }).addTo(mymap);
-        //// curvedPath._path = DOMElement
-
-
-        //curvedPath._path.classList.add("activeStage");
-        //curvedPath._renderer.on('update',
-        //    (e) => {
-        //        let length = curvedPath._path.getTotalLength();
-        //        curvedPath._path.style.strokeDasharray = length;
-        //        curvedPath._path.style.strokeDashoffset = (length) / 4;
-        //        console.log(curvedPath);
-        //        console.log(e);
-        //        console.log(length);
-        //    });
-        //console.log(Point);
-        //console.log(ActiveStage);
         let routeFactory = new RunRouteFactory();
-        let stageTemp1 = {
-            StartPoint: [57.0405, 9.9101],
-            EndPoint: [57.0039, 9.9000],
-            ThroughPoints: [[57.0257, 9.9062], [57.0107, 9.9027]],
-            Status: 'Completed',
-            Sponsor: { Name: "Hoho", Message: "Haha", PictureUrl: "eee.com" },
-            Runners: [
-                { Name: "Julemand", Order: 0, Status: "Completed" },
-                { Name: "Hulemand", Order: 1, Status: "Completed" },
-                { Name: "Kuglemand", Order: 2, Status: "Completed" }
-            ]
-        };
-        let stageTemp2 = {
-            StartPoint: [57.0039, 9.9000],
-            EndPoint: [56.9639, 9.8890],
-            ThroughPoints: [[56.9857, 9.8960], [56.9757, 9.8920]],
-            Status: 'Active',
-            Sponsor: { Name: "Hoho", Message: "Haha", PictureUrl: "eee.com" },
-            Runners: [
-                { Name: "Julemand", Order: 0, Status: "Completed" },
-                { Name: "Hulemand", Order: 1, Status: "Active" },
-                { Name: "Kuglemand", Order: 2, Status: "NotStarted" }
-            ]
-        };
-        let stageTemp3 = {
-            StartPoint: [56.9639, 9.8890],
-            EndPoint: [56.9239, 9.8700],
-            ThroughPoints: [[56.9507, 9.8830], [56.9307, 9.8770]],
-            Status: 'NotFinished',
-            Sponsor: { Name: "Hoho", Message: "Haha", PictureUrl: "eee.com" },
-            Runners: [
-                { Name: "Julemand", Order: 0, Status: "NotStarted" },
-                { Name: "Hulemand", Order: 1, Status: "NotStarted" },
-                { Name: "Kuglemand", Order: 2, Status: "NotStarted" }
-            ]
-        };
-        let data = { Name: "RunTogetherRunTest", Stages: [stageTemp1, stageTemp2, stageTemp3] };
-
-        let routeRun = routeFactory.CreateRunRoute(data);
+        let obj = JSON.parse(object);
+        let routeRun = routeFactory.CreateRunRoute(obj);
         routeRun.AddToMap(mymap);
     }
 
-    calculateControlPoints(latlng1, latlng2, direction) {
-        let offsetX = latlng2[1] - latlng1[1],
-            offsetY = latlng2[0] - latlng1[0];
 
-        let r = Math.sqrt(Math.pow(offsetX, 2) + Math.pow(offsetY, 2)),
-            theta = Math.atan2(offsetY, offsetX);
+    //onMapClick() {
+    //    let pointArray = [];  
 
-        let thetaOffset = (11 / 10);
+    //    var popup = L.popup();
 
-        let r2 = (r / 2) / (Math.cos(thetaOffset)), theta2; 
+    //    function onMapClick(e) {
+    //        popup
+    //            .setLatLng(e.latlng)
+    //            .setContent("You clicked the map at " + e.latlng.toString())
+    //            .openOn(mymap);
 
-        if (direction == "right") {
-            theta2 = theta + thetaOffset;
-        } else if (direction == "left") {
-            theta2 = theta - thetaOffset;
-        }
+    //        pointArray.push(e.latlng.toString());
+    //        console.log(pointArray);
+    //    }
 
-        let  midpointX = (r2 * Math.cos(theta2)) + latlng1[1],
-             midpointY = (r2 * Math.sin(theta2)) + latlng1[0];
-
-        let midpointLatLng = [midpointY, midpointX];
-
-        return midpointLatLng; 
-
-    }
-
-    /* A Method to remove markers and lines*/
-    removeMarkersAndLines() {
-        mymap.removeLayer(layerGroup);
-    }
-
-    onMapClick() {
-
-        let pointArray = [];  
-
-        var popup = L.popup();
-
-        function onMapClick(e) {
-            popup
-                .setLatLng(e.latlng)
-                .setContent("You clicked the map at " + e.latlng.toString())
-                .openOn(mymap);
-
-            pointArray.push(e.latlng.toString());
-            console.log(pointArry);
-        }
-
-        mymap.on('click', onMapClick);
-    }
-}
-
-
-   //If the layer group has to be spilt up
-    // addPolyline(latlngs) {
-    //    polyline = L.polyline(this.latlngs, { color: 'red' }).addTo(mymap);
-    //    mymap.fitBounds(polyline.getBounds());
+    //    mymap.on('click', onMapClick);
     //}
-
-
-//export function onMapClick() {
-
-//    var popup = L.popup();
-
-//    function onMapClick(e) {
-//        popup
-//            .setLatLng(e.latlng)
-//            .setContent("You clicked the map at " + e.latlng.toString())
-//            .openOn(mymap);
-//    }
-
-//    mymap.on('click', onMapClick);
-//}
+}
