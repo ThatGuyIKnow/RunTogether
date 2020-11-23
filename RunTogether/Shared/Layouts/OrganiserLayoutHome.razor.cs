f72d3c66b2ca1b07f83d9a5ee605155112cabdfa
Oliver Harold <oliverhsj@hotmail.dk>
2020-11-23 08:53:25 +0100

    Fixed Login

---
+++ RunTogether/Shared/Layouts/OrganiserLayoutHome.razor.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace RunTogether.Shared.Layouts
+{
+    public partial class OrganiserLayoutHome
+    {
+        public string runnerStatus = "You are the active runner";
+
+        /* Keeps track of whether sidebar is collapsed or not. By default not collapsed */
+        bool collapsed = false;
+
+        readonly string sidebarId = "navBarOrgId";
+
+        async Task ToggleSidebar()
+        {
+            collapsed = !collapsed;
+
+            if (collapsed == false) /* Open */
+            {
+                await JSRuntime.InvokeVoidAsync("Main.Sidebar.sidebarToggle",
+                    new { id = sidebarId, attribute = "width", value = "15rem" });
+            }
+            else /* Close */
+            {
+                await JSRuntime.InvokeVoidAsync("Main.Sidebar.sidebarToggle",
+                    new { id = sidebarId, attribute = "width", value = "0" });
+            }
+        }
+
+        public string ID { get; set; }
+
+        protected override void OnParametersSet()
+        {
+            // pull out the "ID" parameter from the route data
+            object id = null;
+            if ((this.Body.Target as RouteView)?.RouteData.RouteValues?.TryGetValue("id", out id) == true)
+            {
+                ID = id?.ToString();
+            }
+
+        }
+
+    }
+}
