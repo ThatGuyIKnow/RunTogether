481f3c927eadbabb04cd43ec2a1e6c5c98bf5c7f
mflp19 <54845694+mflp19@users.noreply.github.com>
2020-11-23 07:20:05 +0100

    add email validator

--- RunTogether/Shared/Forms/CreateNewRunner.razor.cs
+++ RunTogether/Shared/Forms/CreateNewRunner.razor.cs
@@ -1,25 +1,38 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Radzen.Blazor;
 using RunTogether.Areas.Identity;
 using RunTogether.Areas.Identity.Helpers;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace RunTogether.Shared.Forms
 {
     public partial class CreateNewRunner
     {
-
         [Parameter] public Run selectedRun { get; set; }
+
         ApplicationUser runner = new ApplicationUser();
+        RadzenCompareValidator compareValidator;
 
+        public string targetValue { get; set; }
 
-        async public void OnSubmit(string firstName,string lastName, string email, Run selectedRun)
+        async public void OnSubmit(string firstName, string lastName, string email, Run selectedRun)
         {
-            Console.WriteLine(selectedRun.Name + " " + selectedRun.ID);
-            await userCreation.CreateRunner(firstName, lastName, email, selectedRun);
-            Console.WriteLine("YEY!");
-            this.dialogService.Close(true);
+            targetValue = default;
+            if (selectedRun.Runners.Any(r => r.Email == runner.Email) == false)
+            {
+                Console.WriteLine(selectedRun.Name + " " + selectedRun.ID);
+                await userCreation.CreateRunner(firstName, lastName, email, selectedRun);
+                Console.WriteLine("YEY!");
+                this.dialogService.Close(true);
+            }
+            else
+            {
+                targetValue = runner.Email;
+                compareValidator.EditContext.Validate();
+            }
         }
 
         void OnInvalidSubmit()
