4f085b27100a71f278e5c678f1315030bff601e4
Magnus Mathiesen <magnus.mathiesen@yahoo.com>
2020-11-25 10:16:04 +0100

    Changed tab color & whitespace

--- RunTogether/Shared/Etc/NavMenuOrganiserHome.razor.cs
+++ RunTogether/Shared/Etc/NavMenuOrganiserHome.razor.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.JSInterop;
+using RunTogether.Areas.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +11,25 @@ namespace RunTogether.Shared.Etc
 {
     public partial class NavMenuOrganiserHome
     {
-        [Parameter] public string ID { get; set; }
+        [Parameter]
+        public string ID { get; set; }
+
+        bool isSuperAdmin = false;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            AuthenticationState authState = await authProvider.GetAuthenticationStateAsync();
+            try
+            {
+                ApplicationUser user = await userManager.GetUserAsync(authState.User);
+
+                isSuperAdmin = await userManager.IsInRoleAsync(user, IdentityRoleTypes.SuperOrganiser);
+            }
+            catch (Exception e)
+            {
+                isSuperAdmin = false;
+            }
+            StateHasChanged();
+        }
     }
 }
